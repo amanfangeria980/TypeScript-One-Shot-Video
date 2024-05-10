1b9d74fa06c84da39998fefb41d1ed8232cd7f04
Aman Fangeria <aman.fangeria@geekyants.com>
2024-05-10 16:35:14 +0530

    classes, public and private, getter and setters

--- 03purets/dist/index.js
+++ 03purets/dist/index.js
@@ -1,2 +1,37 @@
 "use strict";
-console.log("Aman");
+// class User{
+//     email:string
+//     name:string
+//     private city: string =""
+//     constructor(email:string,name:string){
+//         this.email=email;
+//         this.name=name;
+//     }
+// }
+// const aman = new User("aman@aman.com","Aman")
+class User {
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
+        this.city = "";
+        this.email = email;
+        this.name = name;
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+    deleteToken() {
+        console.log("Token Deleted!");
+    }
+}
+const aman = new User("aman@aman.com", "Aman");
--- 03purets/src/index.ts
+++ 03purets/src/index.ts
@@ -1,1 +1,47 @@
-console.log("Aman")
+// class User{
+//     email:string
+//     name:string
+//     private city: string =""
+//     constructor(email:string,name:string){
+//         this.email=email;
+//         this.name=name;
+//     }
+// }
+
+// const aman = new User("aman@aman.com","Aman")
+
+
+
+class User {
+    private _courseCount=1;
+    private readonly city: string = ""
+    constructor(public email: string,
+        public name: string) {
+        this.email = email;
+        this.name = name;
+    }
+
+    get getAppleEmail():string{
+        return `apple${this.email}`
+    }
+
+    get courseCount():number{
+        return this._courseCount
+    }
+
+    set courseCount(courseNum){
+        if(courseNum<=1){
+            throw new Error("Course count should be more than 1")
+        }
+        this._courseCount=courseNum
+    }
+
+    private deleteToken(){
+        console.log("Token Deleted!")
+    }
+}
+
+const aman = new User("aman@aman.com", "Aman")
+
+
+
